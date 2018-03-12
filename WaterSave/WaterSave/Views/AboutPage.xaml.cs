cf85125b1c61a5257fca73558cac36284eb87e58
LuisPortugal <luisenriquegomesportugal@gmail.com>
2018-03-12 00:26:59 -0300

    System commit

---
+++ WaterSave/WaterSave/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace WaterSave.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
